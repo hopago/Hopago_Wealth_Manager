27e956a0d80afdb8a193926b86ebdcf4fc99375b
hopago <132269386+hopago@users.noreply.github.com>
2024-12-21 22:44:15 +0900

    링크 수정

--- src/app/(marketing)/components/navbar/navbar.tsx
+++ src/app/(marketing)/components/navbar/navbar.tsx
@@ -4,7 +4,7 @@ import { UserButton } from "../user-button";
 
 export const Navbar = () => {
   return (
-    <nav className="fixed top-[4rem] z-30 w-full flex items-center justify-between p-14">
+    <nav className="fixed top-[4rem] z-30 w-full flex items-center justify-between px-14">
       <Logo />
       {/* Only Desktop */}
       <Links />
